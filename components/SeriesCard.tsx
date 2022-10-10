0729db3946e25dc65ead8303cd3d95153806ff3b
Sangho <istrangeho@gmail.com>
2022-10-10 21:23:35 +0900

    update posts

--- components/SeriesCard.tsx
+++ components/SeriesCard.tsx
@@ -6,19 +6,19 @@ interface ISeriesPage {
 }
 export default function SeriesCard({ metaData }: ISeriesPage) {
 	return (
-		<div className="flex my-4 space-x-4 rounded-md shadow-md">
+		<div className="flex my-4 space-x-4 rounded-md shadow-md bg-slate-900">
 			<img className="w-28 h-28 rounded-md" src={metaData.cover_image} alt="" />
 			<div className="mx-2 pb-1 flex flex-col justify-between w-full">
 				<div>
-					<h3 className="text-lg font-bold">
+					<h3 className="text-lg font-bold text-gray-200">
 						<Link href={`/series/${metaData.series}`}>{metaData.title}</Link>
 					</h3>
-					<p className="text-sm mt-2 text-gray-700">{metaData.excerpt}</p>
+					<p className="text-sm mt-2 text-gray-500">{metaData.excerpt}</p>
 				</div>
-				<ul className="flex border-t-2 pt-1 border-gray-200 w-full">
+				<ul className="flex border-t-2 pt-1 border-gray-700 w-full">
 					{metaData.tag.map((t) => (
-						<li key={t} className="text-gray-400 mx-1 text-xs">
-							{t}
+						<li key={t} className="text-indigo-600 mx-1 text-xs">
+							#{t}
 						</li>
 					))}
 				</ul>
--- components/SeriesList.tsx
+++ components/SeriesList.tsx
@@ -12,7 +12,10 @@ export default function SeriesList({
 	return (
 		<div>
 			{metaDataWithTitle.map((data) => (
-				<div className="flex my-4 space-x-4 rounded-md shadow-md">
+				<div
+					key={data.metaData.title}
+					className="flex my-4 space-x-4 rounded-md shadow-md"
+				>
 					<img
 						className="w-28 h-28 rounded-md"
 						src={data.metaData.cover_image}
@@ -20,18 +23,18 @@ export default function SeriesList({
 					/>
 					<div className="mx-2 pb-1 flex flex-col justify-between w-full">
 						<div>
-							<h3 className="text-lg font-bold">
+							<h3 className="text-lg font-bold text-gray-200">
 								<Link href={`/post/${data.fileName}`}>
 									<a>{data.metaData.title}</a>
 								</Link>
 							</h3>
-							<p className="text-sm mt-2 text-gray-700">
+							<p className="text-sm mt-2 text-gray-500">
 								{data.metaData.excerpt}
 							</p>
 						</div>
-						<ul className="flex border-t-2 pt-1 border-gray-200 w-full">
+						<ul className="flex border-t-2 pt-1 border-gray-700 w-full">
 							{data.metaData.tag.map((t) => (
-								<li key={t} className="text-gray-400 mx-1 text-xs">
+								<li key={t} className="text-indigo-600 mx-1 text-xs">
 									{t}
 								</li>
 							))}
